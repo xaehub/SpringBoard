46675c0a647cf73f39fc153ece598762495938a1
xaehub <novxae@gmail.com>
2025-04-01 19:43:27 +0900

    특정 회원 조회(GetMapping)

--- src/main/java/com/example/board/controller/MemberController.java
+++ src/main/java/com/example/board/controller/MemberController.java
@@ -1,16 +1,14 @@
 package com.example.board.controller;
 
 
+import com.example.board.dto.MemberResponseDto;
 import com.example.board.dto.SignUpRequestDto;
 import com.example.board.dto.SignUpResponseDto;
 import com.example.board.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/members")
@@ -27,5 +25,13 @@ public class MemberController {
         return new ResponseEntity<>(signUpResponseDto, HttpStatus.CREATED);
 
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MemberResponseDto> findById(@PathVariable Long id) {
+        MemberResponseDto memberResponseDto = memberService.findById(id);
+
+        return new ResponseEntity<>(memberResponseDto, HttpStatus.OK);
+    }
+
 
 }
--- src/main/java/com/example/board/service/MemberService.java
+++ src/main/java/com/example/board/service/MemberService.java
@@ -1,10 +1,15 @@
 package com.example.board.service;
 
+import com.example.board.dto.MemberResponseDto;
 import com.example.board.dto.SignUpResponseDto;
 import com.example.board.entity.Member;
 import com.example.board.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +24,17 @@ public class MemberService {
         Member savedMember = memberRepository.save(member);
 
         return new SignUpResponseDto(savedMember.getId(), savedMember.getUsername(), savedMember.getAge());
+    }
+
+    public MemberResponseDto findById(Long id) {
+        Optional<Member> optionalMember = memberRepository.findById(id);
+
+        if (optionalMember.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exists id : " + id);
+        }
+
+        Member findMember = optionalMember.get();
+
+        return new MemberResponseDto(findMember.getUsername(), findMember.getAge());
     }
 }
